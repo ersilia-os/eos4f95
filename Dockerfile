1e6f8c99ad7dc75dde520e4f8b79d9ebcfd53e7b
gemmaturon <gemma@ersilia.io>
2025-11-19 13:43:07 +0100

    add model lq v2

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,7 @@
-FROM bentoml/model-server:0.11.0-py310
+FROM bentoml/model-server:0.11.0-py312
 MAINTAINER ersilia
 
-RUN pip install lightgbm==3.3.5
-RUN pip install xgboost==1.7.5
-RUN pip install lazyqsar==0.3 
-RUN pip install scikit-learn==1.2.2
+RUN pip install lazyqsar[descriptors]==2.1.1
 
 WORKDIR /repo
 COPY . /repo
--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,38 +1,35 @@
+# imports
 import os
 import csv
 import sys
-import joblib
+from lazyqsar.qsar import LazyBinaryQSAR
 
-# parse arguments
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-# current file directory
 root = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.abspath(os.path.join(root, "..", "..", "checkpoints"))
 
-MODELPATH = os.path.join(root, "..", "..", "checkpoints")
-
-
-# my model
-def my_model(smiles_list):
-    mdl1 = joblib.load(os.path.join(MODELPATH, "mycetos_morgan_model.joblib"))
-    y_pred1 = mdl1.predict_proba(smiles_list)[:,1]
-
-    return y_pred1
-
-
-# read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
     reader = csv.reader(f)
     next(reader)  # skip header
     smiles_list = [r[0] for r in reader]
 
-# run model
-output1 = my_model(smiles_list)
+def predict(smiles_list, model_path):
+    model = LazyBinaryQSAR.load(model_path)
+    y_hat = model.predict_proba(smiles_list=smiles_list)[:, 1]
+    return y_hat
+
+
+outputs = predict(smiles_list, model_path)
+
+input_len = len(smiles_list)
+output_len = len(outputs)
+assert input_len == output_len
 
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["mycetoma_inhibition"])  # header with column names
-    for o1 in output1:
-        writer.writerow([o1])
+    writer.writerow(["mycetoma_inhibition_probability"])
+    for o in outputs:
+        writer.writerow([o])
